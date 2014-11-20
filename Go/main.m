03ea5b7cbec127a4c5b6b24c24524c4473623331
livoras <livoras@163.com>
2014-11-20 17:17:03 +0800

    Initial Commit

---
+++ Go/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Go
+//
+//  Created by 戴嘉华 on 14/11/20.
+//  Copyright (c) 2014年 戴嘉华. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
